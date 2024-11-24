5ffc735600acdb9edf79bb24db240d8766ba6f02
Marcia <juddhope@gmail.com>
2024-11-24 14:07:13 -0600

    Add stashed changes after rebase.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
 
   # Defines available roles and sets default role as user.
-  ROLES = %w[admin user].freeze
+  # Add teacher to roles
+  ROLES = %w[admin user teacher].freeze
   has_many :kit_requests
   has_many :donations, -> { where(canceled: false) }
   has_many :contacts
