afac887811a37e4b51bd25ba7cc102e007f9bd79
Marcia <juddhope@gmail.com>
2024-11-26 03:52:31 -0600

    Modified user_profile_serializer for better readability and modularity, removed unecessary comments

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::UsersController < ApplicationController
 load_and_authorize_resource
 before_action :set_user, only: [ :update, :show ]
 
-  # Using this controller to alter User model, but this functionality could be transferred to Devise built in controllers
   # GET /api/v1/users
   def index
     @users = User.all
@@ -13,16 +12,6 @@ before_action :set_user, only: [ :update, :show ]
   def show
     render json: @users
   end
-
-  # The create method is handled by Devise users/registration
-  # def create
-  #   @user = User.new(user_params)
-  #   if @user.save
-  #     render json: { message: "User created successfully!" }, status: :created
-  #   else
-  #     render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
-  #   end
-  # end
 
   # PATCH/PUT api/v1/users/1
   def update
@@ -38,7 +27,8 @@ before_action :set_user, only: [ :update, :show ]
     @user.destroy
     render json: { message: "User deleted successfully!" }
   end
-  # Add profile action that renderd json: UserProfileSerializer and pass current_user as params for conditional statement in serializer
+
+  # Add profile action and pass current_user as params
   def profile
     render json: UserProfileSerializer.new(current_user, { params: { current_user: current_user } })
   end
@@ -52,5 +42,4 @@ before_action :set_user, only: [ :update, :show ]
   def user_params
   params.require(:user).permit(:first_name, :last_name, :role)
   end
-  # Noticed my end was in the wrong place
 end
--- app/serializers/user_profile_serializer.rb
+++ app/serializers/user_profile_serializer.rb
@@ -1,11 +1,17 @@
 class UserProfileSerializer < ActiveModel::Serializer
   attributes :name, :email
 
+  # handle nil values
   attribute :donations do |user|
-    DonationSerializer.new(user.donations)
+    user.donations ? DonationSerializer.new(user.donations) : []
   end
 
-  attribute :orders, if: Proc.new { |user, params| params[:current_user].teacher? } do | user |
-    OrderSerializer.new(user.orders)
+  attribute :orders, if: :include_orders? do |user|
+    user.orders ? OrderSerializer.new(user.orders) : []
+  end
+
+  # check if the user is a teacher
+  def include_orders?(user, params)
+    params[:current_user].teacher?
   end
 end
