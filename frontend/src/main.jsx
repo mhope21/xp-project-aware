fc8be45999ad9c4c7e6870b5e9f39d5a31dea755
Marcia <juddhope@gmail.com>
2024-12-09 22:06:46 -0600

    Trying to fix retained token bug

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App.jsx'
 import { AuthProvider } from './components/auth/AuthContext.jsx'
 import './index.css'
