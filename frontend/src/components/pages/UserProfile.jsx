5fe2c157313d87eeb4fb8f3a41d8b1daf03f7043
Marcia <juddhope@gmail.com>
2024-12-19 09:08:47 -0600

    Needed to import link on UserProfile and added link to profile

--- frontend/src/components/pages/UserProfile.jsx
+++ frontend/src/components/pages/UserProfile.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { API_URL } from '../../constants';
+import { Link } from 'react-router-dom';
 
 const UserProfile = () => {
   const [profile, setProfile] = useState(null);
