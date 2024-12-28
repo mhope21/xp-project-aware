8fd62c2753b305c65c447b264016fb746594e2db
Tracy Cano <tracycano3.14@gmail.com>
2024-12-27 17:52:00 -0800

    creating event model

---
+++ app/models/event.rb
@@ -0,0 +1,7 @@
+class Event < ApplicationRecord
+  belongs_to :speaker, -> { where(role: 'speaker') }, class_name: 'User'
+  has_many :bookings, dependent: :destroy
+
+  validates :title, presence: true
+  validates :duration, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_24_035858) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_28_014803) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -62,6 +62,16 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_24_035858) do
     t.string "stripe_checkout_session_id"
     t.string "stripe_payment_intent_id"
     t.index ["user_id"], name: "index_donations_on_user_id"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.integer "speaker_id", null: false
+    t.string "title", null: false
+    t.string "description"
+    t.integer "duration", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["speaker_id"], name: "index_events_on_speaker_id"
   end
 
   create_table "kit_items", force: :cascade do |t|
@@ -119,6 +129,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_24_035858) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "contacts", "users"
   add_foreign_key "donations", "users"
+  add_foreign_key "events", "users", column: "speaker_id"
   add_foreign_key "orders", "kits"
   add_foreign_key "orders", "users"
 end
