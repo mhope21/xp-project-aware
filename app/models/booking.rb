d66417f95dca7281910b7c4af1a0f65da53691f1
Marcia <juddhope@gmail.com>
2025-01-02 00:23:21 -0600

    Add enum and validations for booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,10 @@ class Booking < ApplicationRecord
   has_one :speaker, through: :event
   has_one :order, as: :product
   has_one :user, through: :order
+
+  enum status: { pending: 0, confirmed: 1, denied: 2 }
+
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :status, presence: true
 end
