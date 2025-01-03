8ca0ee52390da7d01ae0e686e996464cb180f44c
Olesia Mironenko <olesia.a.mironenko@gmail.com>
2025-01-03 11:06:59 -0500

    HER-38 lint fixed

--- spec/factories/availabilities.rb
+++ spec/factories/availabilities.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :availability do
     start_time { Faker::Time.forward(days: 7, period: :morning) }
     end_time { start_time + 1.hour }
-    association :speaker, factory: [:user, :speaker]
+    association :speaker, factory: [ :user, :speaker ]
     association :recurring_availability
   end
 end
