680d5555c3c5717038a64603223004f4609443ce
Marcia <juddhope@gmail.com>
2025-01-03 14:33:22 -0600

    Fix lint errors after merge conflict resolution

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -18,11 +18,10 @@ FactoryBot.define do
     trait :speaker_user do
       role { "speaker" }
     end
-    
+
     # To include address-user associations
     after(:build) do |user|
       user.addresses << build(:address, addressable: user)
-
     end
   end
 end
