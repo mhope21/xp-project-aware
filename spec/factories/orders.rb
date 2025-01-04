9b4fe0fd9b115786c2c699dbcf9e300d6e655141
Dewi-Anggraini <nn.dewi.anggraini@gmail.com>
2025-01-04 10:37:45 +0700

    resolving conflict

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -3,9 +3,11 @@ FactoryBot.define do
     school_year { "2025-2026" }
     phone { "#{rand(100..999)}-#{rand(100..999)}-#{rand(1000..9999)}" }
     comments { "This is wonderful" }
+    # define products (Kit, Donation, event) as nil
     transient do
       product {nil}
     end
+    # add comment here
     after(:build) do |order, evaluator|
           order.product = evaluator.product
     end
