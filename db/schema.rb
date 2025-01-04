40aa3b5b87486f7ae6d99b2507838ea3784e356c
Marcia <juddhope@gmail.com>
2025-01-04 13:28:11 -0600

    Fixed problems on main

--- db/schema.rb
+++ db/schema.rb
@@ -174,7 +174,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_04_173639) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "bookings", "events"
   add_foreign_key "availabilities", "recurring_availabilities"
   add_foreign_key "availabilities", "users", column: "speaker_id"
   add_foreign_key "bookings", "events"
