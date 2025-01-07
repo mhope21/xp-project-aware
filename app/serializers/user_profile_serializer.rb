ce301c4ef0f60d72da45d3f8c12d122518abc300
Tracy Cano <tracycano3.14@gmail.com>
2025-01-06 18:12:40 -0800

    changed back to def from attribute

--- app/serializers/user_profile_serializer.rb
+++ app/serializers/user_profile_serializer.rb
@@ -11,9 +11,10 @@ class UserProfileSerializer
   end
 
   # Added the profile_image_url method here
-  attribute :profile_image_url do |user|
-    if user.profile_image.attached?
-      Rails.application.routes.url_helpers.rails_blob_url(user.profile_image, only_path: true)
+
+    def profile_image_url
+    if object.profile_image.attached?
+      Rails.application.routes.url_helpers.rails_blob_url(object.profile_image, only_path: true)
     else
       ActionController::Base.helpers.asset_path("default_profile_image.png")
     end
