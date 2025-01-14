29fd38bd0def05f70a052df41772314d9c829e75
Dewi-Anggraini <nn.dewi.anggraini@gmail.com>
2025-01-14 10:09:09 +0700

    added new migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_10_130617) do
 ActiveRecord::Schema[7.2].define(version: 2025_01_11_165839) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -96,6 +95,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_11_165839) do
     t.datetime "updated_at", null: false
     t.string "payment_token"
     t.boolean "canceled", default: false
+    t.string "stripe_checkout_session_id"
+    t.string "stripe_payment_intent_id"
     t.index ["user_id"], name: "index_donations_on_user_id"
   end
 
@@ -136,9 +137,9 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_11_165839) do
     t.string "phone"
     t.text "comments"
     t.integer "user_id"
+    t.string "product_type", null: false
+    t.integer "product_id", null: false
     t.integer "address_id"
-    t.string "product_type"
-    t.integer "product_id"
     t.index ["product_type", "product_id"], name: "index_orders_on_product"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
