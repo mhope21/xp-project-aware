a052d9de0afe4b06f99ab6c231b6bff0b0f20df2
Olesia Mironenko <olesia.a.mironenko@gmail.com>
2025-01-13 22:48:21 -0500

    run migration after main update

--- db/schema.rb
+++ db/schema.rb
@@ -95,8 +95,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_11_165839) do
     t.datetime "updated_at", null: false
     t.string "payment_token"
     t.boolean "canceled", default: false
-    t.string "stripe_checkout_session_id"
-    t.string "stripe_payment_intent_id"
     t.index ["user_id"], name: "index_donations_on_user_id"
   end
 
@@ -137,9 +135,9 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_11_165839) do
     t.string "phone"
     t.text "comments"
     t.integer "user_id"
-    t.string "product_type", null: false
-    t.integer "product_id", null: false
     t.integer "address_id"
+    t.string "product_type"
+    t.integer "product_id"
     t.index ["product_type", "product_id"], name: "index_orders_on_product"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
